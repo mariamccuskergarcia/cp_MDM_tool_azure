fbb16b9d48f4e2f27424d3dedf7ada41ba4e223a
Maria McCusker-Garcia <168100524+mariamccuskergarcia@users.noreply.github.com>
2024-05-16 12:50:31 +0100

    Update outputs.tf

--- mdm_training_tool_azure/outputs.tf
+++ mdm_training_tool_azure/outputs.tf
@@ -1,4 +1,6 @@
 
+#  Streamlit is a Python library used for creating web applications for data science and machine learning projects
+
 output "web_app_name" {
   value = module.streamlit_app.web_app_name
 }
